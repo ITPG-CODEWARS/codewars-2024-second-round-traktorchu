33b106a7aa21f112a52a039f64692b179342ba60
traktorchu <ioniasen43@gmail.com>
2024-11-09 22:10:37 +0200

    Fixed tailwind + daisyui + db test and init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./src/**/*.{html,js}", // Adjust the paths based on where your files are located
+    "./index.html",
+  ],
+  plugins: [
+    require('daisyui'),  // Add this line to include DaisyUI
+  ],
 }
 
